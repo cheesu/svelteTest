df693d88c739cfa4568c0161d8001b1f4d0e54c6
cheesu <dlscltn@gmail.com>
2023-02-16 15:59:40 +0900

    add test page

---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,31 @@
+<svelte:head>
+	<title>About</title>
+	<meta name="description" content="About this app" />
+</svelte:head>
+
+<script>
+
+
+</script>
+
+<div class="text-column">
+	<h1>테스트 페이지</h1>
+
+	<p>
+		This is a <a href="https://kit.svelte.dev">SvelteKit</a> app. You can make your own by typing the
+		following into your command line and following the prompts:
+
+		이런식으로 가는거구나
+	</p>
+
+	<pre>타입스크립트 처음 써보는데 드럽게 어렵네요</pre>
+
+	<p>
+		에베베베베베베
+	</p>
+
+	<p>
+		The <a href="/sverdle">Sverdle</a> page illustrates SvelteKit's data loading and form handling. Try
+		using it with JavaScript disabled!
+	</p>
+</div>
